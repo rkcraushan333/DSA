ec2dc1c126d2eb16dae0e348ee1a54549f93bd77
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2022-03-05 15:39:41 +0530

    ..

--- Concepts/recursion/permut_C_case.cpp
+++ Concepts/recursion/permut_C_case.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-void permut(string s, int n, int i = 1)
+void permut(string s, int i = 0)
 {
-    if (n == 0)
+    if (i == s.length())
     {
         cout << s << endl;
         return;
     }
     string op1, op2;
     op1 = s;
-    op2 = s.insert(i, "_");
-    permut(op1, n - 1, i + 1);
-    permut(op2, n - 1, i + 2);
+    s[i] -= 32;
+    op2 = s;
+    permut(op1, i + 1);
+    permut(op2, i + 1);
 }
 int main()
 {
 
     string s;
     cin >> s;
-    permut(s, s.length() - 1);
+    permut(s);
     return 0;
 }
