6a247c7abe5bcb773d1db08bd9b42b49fdb5ddae
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2022-03-07 22:32:44 +0530

    .

--- Concepts/Tree/level_order_traversal_2.cpp
+++ Concepts/Tree/level_order_traversal_2.cpp
@@ -12,24 +12,26 @@ struct node
         left = right = NULL;
     }
 };
-int height(node *root)
-{
-    if (root == NULL)
-        return 0;
 
-    return max(height(root->left), height(root->right)) + 1;
-}
-void print_level(node *root, int n)
+void print_level(node *root)
 {
     if (root == NULL)
         return;
-    if (n == 0)
+
+    queue<node *> q;
+    q.push(root);
+
+    while (!q.empty())
     {
-        cout << root->key << " ";
-        return;
+        node *curr = q.front();
+        cout << curr->key << " ";
+        q.pop();
+        if (curr->left != NULL)
+            q.push(curr->left);
+
+        if (curr->right != NULL)
+            q.push(curr->right);
     }
-    print_level(root->left, n - 1);
-    print_level(root->right, n - 1);
 }
 int main()
 {
@@ -41,10 +43,6 @@ int main()
     root->left->right = new node(7);
     root->left->right->left = new node(9);
     root->left->right->right = new node(15);
-    int k = height(root);
-    for (int i = 0; i < k; i++)
-    {
-        print_level(root, i);
-    }
+    print_level(root);
     return 0;
 }
