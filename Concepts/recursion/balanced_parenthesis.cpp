39079c3cc45573dbf4abf5ebaabac3a21b318a31
rkcraushan333 <raushank.ug20.ece@nitp.ac.in>
2022-03-07 15:09:44 +0530

    .

--- Concepts/recursion/balanced_parenthesis.cpp
+++ Concepts/recursion/balanced_parenthesis.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int i = 0;
 
-void balance(int n, int op, int cl, string s = "")
+void balance(int op, int cl, string s = "")
 {
-    if (s.length() == (2 * n))
+    if (op == 0 && cl == 0)
     {
         cout << ++i << s << endl;
         return;
     }
     if (op >= cl)
     {
-        balance(n, op - 1, cl, s + "(");
+        balance(op - 1, cl, s + "(");
     }
 
     else
     {
         if (op != 0)
         {
-            balance(n, op - 1, cl, s + "(");
+            balance(op - 1, cl, s + "(");
         }
-        balance(n, op, cl - 1, s + ")");
+        balance(op, cl - 1, s + ")");
     }
 }
 
@@ -30,6 +30,6 @@ int main()
 
     int n;
     cin >> n;
-    balance(n, n, n);
+    balance(n, n);
     return 0;
 }
